b4fdbf1506c7d5d6f3ffccaf4d54e331fd8cfc94
neilmyers <mmyersneil@gmail.com>
2019-10-18 14:43:55 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import request from "superagent"
 
-function App() {
+const App = () => {
+  const [upFile, setUpFile] = React.useState(null)
+  const [pic, setPic] = React.useState()
+
+  const fileSelected = e => {
+    setUpFile(e.target.files[0])
+  }
+
+  const uploadFile = file => {
+    let upload = request
+      .post("https://api.cloudinary.com/v1_1/thewatchfox/image/upload")
+      .field("upload_preset", "meme-images")
+      .field("file", file)
+
+    upload.end((err, response) => {
+      if (err) {
+        console.log("error uploading to Cloudinary", file)
+      }
+      if (response.body.secure_url !== "") {
+        console.log(response.body.secure_url)
+        setPic(response.body.secure_url)
+      }
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="file" onChange={fileSelected} />
+      <button onClick={() => uploadFile(upFile)}>Upload</button>
+
+      <img src={pic} alt="upload" />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
